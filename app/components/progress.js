b97fd33b30f30ac72e7738b8c1200561944d38f4
sunqiaojie <437192731@qq.com>
2018-01-11 00:29:26 +0800

    添加进度条

--- app/components/progress.js
+++ app/components/progress.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './progress.less'
 
 
 
@@ -6,15 +7,31 @@ class Progress extends React.Component {
     constructor(props) {
     	super(props);
     }
+  /*  getDefaultProps(){
+    	return {
+    		barColor: '#2f9842'
+    	}
+    }*/
+   
+    changeProgress(e){
+    	let progressBar = this.refs.progressBar;
+    	let progress = (e.clientX - progress.getBoundingClientRect().left)/(progressBar.clientWidth);
+    	this.props.onProgressChange && this.props.onProgressChange(progress);
+    }
     componentDidMount() {}
     componentWillUnmount() {}
     render(){
     	return (
-    		<div className="components-progress row">
-    			{this.props.progress}s
-    		</div>
+    		<div className="components-progress" ref="progressBar" onClick={this.changeProgress}>
+                <div className="progress" style={{width:`${this.props.progress}%`,background:this.props.barColor}}>
+
+                </div>
+            </div>
     	)
     }
+}
+Progress.defaultProps = {
+		barColor : '#2f9842'
 }
 
 export default Progress;
